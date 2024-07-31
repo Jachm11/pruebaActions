523174ca7ee71a2c29ee955cff752ea84cd13d5e
Jachm <joalchama@gmail.com>
2024-07-31 15:13:57 -0600

    test

---
+++ main.sv
@@ -0,0 +1,12 @@
+// Simple AND gate module in Verilog
+
+module and_gate (
+    input  wire a,  // First input
+    input  wire b,  // Second input
+);
+
+      assign y = a & b;  // Output is the AND of inputs a and b
+
+
+
+endmodule
